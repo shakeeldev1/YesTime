e01bf5a0676eb79ddfc7cee072cf9df1a70c09ce
shakeel7521951 <shakeel7521951@gmail.com>
2026-02-04 15:43:17 +0500

    feat: implement authentication module with JWT and Passport strategies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '24h' }
+    })
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtModule]
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { SignupDto } from './dto/signup.dto';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private userService: UsersService, private jwtService: JwtService) { }
+
+    async signup(dto: SignupDto) {
+        const exists = await this.userService.findByPhone(dto.phone);
+        if (exists) throw new BadRequestException('User with this phone number already exists');
+        const hashedPassword = await bcrypt.hash(dto.password, 10);
+        const user = await this.userService.createUser({
+            ...dto,
+            password: hashedPassword
+        })
+        return this.signToken(user._id.toString());
+    }
+
+    async login(phone:string,password:string){
+        const user = await this.userService.findByPhone(phone);
+        if(!user) throw new BadRequestException('Invalid phone number or password');
+        const passwordMatches = await bcrypt.compare(password,user.password);
+        if(!passwordMatches) throw new BadRequestException('Invalid phone number or password');
+        return this.signToken(user._id.toString());
+    }
+
+
+    async signToken(userId: string) {
+        const payload = { sub: userId };
+        const token = await this.jwtService.signAsync(payload, {
+            expiresIn: '7d',
+        });
+        return {
+            access_token: token,
+        };
+    }
+}
