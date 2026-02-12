0d4cf27601a787e1acc2b6894225bd5ff11d3d54
shakeel7521951 <shakeel7521951@gmail.com>
2026-02-12 16:11:38 +0500

    feat: add Mongoose integration for Wallet and Transaction schemas in wallet module

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { WalletController } from './wallet.controller';
 import { WalletService } from './wallet.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Wallet, WalletSchema } from './schemas/wallet.schema';
+import { Transaction, TransactionSchema } from './schemas/transaction.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Wallet.name, schema: WalletSchema },
+      { name: Transaction.name, schema: TransactionSchema }
+    ])
+  ],
   controllers: [WalletController],
   providers: [WalletService]
 })
