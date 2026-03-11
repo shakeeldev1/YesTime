08762fab9839607ed16e37ac90fb2a3530ac1433
shakeel7521951 <shakeel7521951@gmail.com>
2026-03-11 11:41:44 +0500

    fix(main): update default port to 3000 in bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // Enable CORS for client requests
-  await app.listen(process.env.PORT ?? 5000);
-  console.log(`Application is running on: http://localhost:${process.env.PORT ?? 5000}`);
+  await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: http://localhost:${process.env.PORT ?? 3000}`);
 }
 bootstrap();
